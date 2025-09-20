05e1485f936a4870ef21bc199c7e2b0e51dbb740
Sanzid Ahmed <sanzidahmed313@gmail.com>
2025-09-20 16:21:38 +0600

    first commit

---
+++ src/Components/Countries/Countries.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Countries = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Countries;
+
+
+
+// Vs code. 
+// Install react extrension package. 
+// shortcut rsc. 
