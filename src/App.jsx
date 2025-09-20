b6b56109b32625c86faaa8bbb00d5f79cf35bd74
Sanzid Ahmed <sanzidahmed313@gmail.com>
2025-09-20 16:52:42 +0600

    added secons one

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,44 @@
 import './App.css'
+import React, { Suspense } from 'react';
 import Countries from './Components/Countries/Countries'
+
+const countries = fetch("https://openapi.programming-hero.com/api/all")
+.then(response => response.json());
 
 function App() {
   return (
     <>
-      <Countries></Countries>
+    <Suspense fallback= {<h1>Loading.....</h1>}>
+      <Countries countries = {countries}></Countries>
+    </Suspense>
+      
     </>
   )
 }
 
 export default App
+
+
+
+// Test => 
+/*
+    1. fetch why here?
+
+*/
+
+
+
+
+// New learn => 
+/*
+    1. Suspense = It is loading. 
+    2. I need to import Suspance. 
+*/
+
+
+
+
+// Steps => 
+/*
+  1. 
+*/
--- src/Components/Countries/Countries.jsx
+++ src/Components/Countries/Countries.jsx
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, { use } from 'react';
 
-const Countries = () => {
+const Countries = ({countries}) => {
+    // console.log(countries);
+    const countriesData = use(countries);
+    const countrieS = countriesData.countries;
+    console.log(countrieS);
     return (
         <div>
             
@@ -15,3 +19,18 @@ export default Countries;
 // Vs code. 
 // Install react extrension package. 
 // shortcut rsc. 
+
+
+
+// New learn => 
+/*
+    1. parameters must be dynamic. 
+*/
+
+
+
+
+// Steps => 
+/*
+  1. 
+*/
